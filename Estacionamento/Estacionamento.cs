27fdaaf5cf6fe79ace0b3b7a9dcab0418bde020c
João Paulo <joaopaulo_marquesj@outlook.com>
2022-10-04 17:54:36 -0300

    Desafio .net fundamentos

---
+++ Estacionamento/Estacionamento.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento
+{
+    internal class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoHora = 0;
+
+        private List<string> listaPlacas = new List<string> { };//declarando nossa lista para armazenarmos as placas
+      public Estacionamento(decimal precoInicial, decimal precoHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoHora = precoHora;
+        }
+        
+        public void adicionarPlacas()
+        {
+            Console.WriteLine("Digite a placa do carro que será estacionado: ");
+             listaPlacas.Add(Console.ReadLine());
+            Console.WriteLine("Placa cadastrada");
+        }
+        //remover
+        public void removerPlacas()
+        {
+            Console.WriteLine("Digite a placa do veiculo a ser retirado: ");
+            string placa = Console.ReadLine();
+            if (listaPlacas.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veiculo ficou estacionado: ");
+                int horas = int.Parse(Console.ReadLine());
+                decimal valorFinal = (precoHora * horas) + precoInicial; 
+                Console.WriteLine("A placa "+placa+" foi removida e o valor a ser cobrado é de: "+valorFinal);
+                listaPlacas.Remove(placa);
+            }
+            else
+            {
+                Console.WriteLine("Não temos um veículo com esta placa, estacionado! Digite uma placa valida.");
+            }
+        }
+
+
+        public void listarPlacas()
+        {
+            if (listaPlacas.Any())
+                { int contadorForeach = 0;
+                    foreach(string placa in listaPlacas)
+                     {
+                    Console.WriteLine("Temos estacionados os veiculos de placa: " + placa);
+                    contadorForeach++;
+                }
+            } else { Console.WriteLine("Não há placas cadastradas! "); }
+        }
+
+
+
+    }
+}
---
+++ Estacionamento/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Estacionamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal precoInicial = 0;
+            decimal precoHora = 0;
+            bool exibirMenu = true;
+            Console.WriteLine("digite o valor inicial: ");
+            precoInicial = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Digite o valor da hora: ");
+            precoHora = Convert.ToDecimal(Console.ReadLine());
+            Estacionamento est = new Estacionamento(precoInicial,precoHora);
+            while (exibirMenu)
+            {
+                Console.WriteLine("Digite sua opção: ");
+                Console.WriteLine("\n1 - Cadastrar Veículo");
+                Console.WriteLine("\n2 - Remover Veículo");
+                Console.WriteLine("\n3 - Listar Veículos");
+                Console.WriteLine("\n4 - Encerrar");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        est.adicionarPlacas();
+                        break;
+                    case "2":
+                        est.removerPlacas();
+                        break;
+                    case "3":
+                        est.listarPlacas();
+                        break;
+                    case "4":
+                        exibirMenu = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção invalida");
+                        break;
+                }
+            }
+        }
+    }
+}
